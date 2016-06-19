1531f27b7aa0f25fd5753f1b26d6bf9f792d77f5
Stanleyr001 <stanleyr001@gmail.com>
2016-06-19 12:59:55 -0700

    RESTLogin created

--- app/src/main/java/com/topicplaces/android/androidsnsapp/AndroidSNS/AndroidSNS.java
+++ app/src/main/java/com/topicplaces/android/androidsnsapp/AndroidSNS/AndroidSNS.java
@@ -1,9 +1,52 @@
 package com.topicplaces.android.androidsnsapp.AndroidSNS;
 
-public class AndroidSNS {
+import com.topicplaces.android.androidsnsapp.AndroidSNS.Users.RESTLogin;
+
+import java.io.IOException;
+import java.net.*;
+
+public class AndroidSNS{
+
     private String ENDPOINT;
 
-    public AndroidSNS(String end){
-        ENDPOINT = end;
+    /**
+     *
+     * Checks that a connection can be made with the endpoint.
+     * If not, program stalls until a connection can be reestablished.
+     *
+     */
+    private void ensureConnection() {
+
+        boolean connected = false;
+
+        while ( !connected ) {
+            try {
+                URLConnection connection = new URL( ENDPOINT ).openConnection();
+                connection.connect();
+
+                connected = true;
+                //System.out.println("Connection to " + ENDPOINT + " established.");
+            } catch ( MalformedURLException e ) {
+                throw new IllegalStateException("Bad URL: " + ENDPOINT, e);
+            } catch ( IOException e ) {
+
+                System.out.println( "Connection to " + ENDPOINT + " lost, attempting to reconnect..." );
+
+                try {
+                    Thread.sleep( 15000 );
+                } catch ( InterruptedException s ) {
+                    throw new IllegalStateException( "Sleep interrupted.");
+                }
+            }
+        }
     }
+
+
+    public String acquireKey( String username, String password) {
+        ensureConnection();
+
+        RESTLogin logg = new RESTLogin(ENDPOINT);
+        return logg.login( username, password );
+    }
+
 }
--- app/src/main/java/com/topicplaces/android/androidsnsapp/AndroidSNS/Users/RESTLogin.java
+++ app/src/main/java/com/topicplaces/android/androidsnsapp/AndroidSNS/Users/RESTLogin.java
@@ -2,4 +2,14 @@ package com.topicplaces.android.androidsnsapp.AndroidSNS.Users;
 
 public class RESTLogin {
 
+    private String ENDPOINT;
+
+    public RESTLogin(String end) {
+        ENDPOINT = end;
+    }
+
+    public String login(String users, String passwords){
+        return users + passwords;
+    }
+
 }
